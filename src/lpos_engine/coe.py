5fd406bf2a224f6082d848a0e574fe427d5615f2
Daniel Steele <dan@Mac.attlocal.net>
2026-08-06 12:32:40 -0500

    Update COE server version to 4.7.0

--- src/lpos_engine/coe.py
+++ src/lpos_engine/coe.py
@@ -128,7 +128,7 @@ def _escape(value: str) -> str:
 
 
 class COEHandler(BaseHTTPRequestHandler):
-    server_version = "LPOS-COE/4.6.0"
+    server_version = "LPOS-COE/4.7.0"
 
     @property
     def app(self) -> "COEServer":
